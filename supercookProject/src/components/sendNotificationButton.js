6f5bc35c30be376c59b98543830f163c2c0fcb85
Nursah <nursah.kara@advancity.com.tr>
2020-08-30 23:58:49 +0300

    background task changes

--- supercookProject/src/components/sendNotificationButton.js
+++ supercookProject/src/components/sendNotificationButton.js
@@ -4,45 +4,59 @@ import signalr from 'react-native-signalr';
 import PushNotification from 'react-native-push-notification';
 import BackgroundTask from 'react-native-background-task';
 
-BackgroundTask.define(() => {
-  console.log('Hello from a background task');
-  PushNotification.localNotification({
-    title: "notification",
-    message: "Hello from a background task",
-});
-  BackgroundTask.finish();
-})
-const connection = signalr.hubConnection('http://192.168.43.250/NotificationWebService');
+const connection = signalr.hubConnection('http://192.168.1.100/NotificationWebService');
 connection.logging = true;
 const notificationHub = connection.createHubProxy('notificationHub');
 notificationHub.on("ReceiveNotification", function (notification) {
     console.log(notification);
     PushNotification.localNotification({
-        title: notification,
-        message: notification,
+        title: notification.title,
+        message: notification.message,
         vibrate: true
     });
+    notificationHub.invoke('DeleteNotifications')
+        .fail(() => {
+            console.warn('error when calling DeleteNotifications')
+        })
 });
-export default class SendNotification extends React.Component {
-    componentDidMount() {
-    BackgroundTask.schedule();
-    console.disableYellowBox = true;
+BackgroundTask.define(() => {
+    console.log('Hello from a background task');
+    notificationHub.on("ReceiveNotification", function (notification) {
+        console.log(notification);
+        PushNotification.localNotification({
+            title: notification.title,
+            message: notification.message,
+            vibrate: true
+        });
+        notificationHub.invoke('DeleteNotifications')
+            .fail(() => {
+                console.warn('error when calling DeleteNotifications')
+            })
+        BackgroundTask.finish();
+    });
     connection.start().done(() => {
         console.log('Now connected, connection ID=' + connection.id);
     }).fail(() => {
         console.log('Failed');
     });
-}
+})
+export default class SendNotification extends React.Component {
+    componentDidMount() {
+        BackgroundTask.schedule();
+        console.disableYellowBox = true;
+        connection.start().done(() => {
+            console.log('Now connected, connection ID=' + connection.id);
+        }).fail(() => {
+            console.log('Failed');
+        });
+    }
     render() {
         return (
-            <View style={{marginTop:100}}>
-                <Button onPress={()=>{
-                    notificationHub.invoke('SendNotification')
-                    .fail(() => {
-                        console.warn('error when calling SendNotification')
-                    })
+            <View style={{ marginTop: 100 }}>
+                <Button onPress={() => {
+                    console.log("send");
                 }}
-                title="send"/>
+                    title="send" />
             </View>
         )
     }
